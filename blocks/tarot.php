838812df8696f981c244b96c53dc1edd93949b02
George Stephanis <George@Stephanis.info>
2018-04-10 15:35:16 -0400

    Strip out extra css file

--- blocks/tarot.php
+++ blocks/tarot.php
@@ -3,7 +3,7 @@
 function tarot_enqueue_block_editor_assets() {
 	$dir = dirname( __FILE__ );
 	$block_js = 'tarot/block.js';
-	$editor_css = 'tarot/editor.css';
+	$editor_css = '../tarot.css';
 	wp_enqueue_script( 'tarot-block', plugins_url( $block_js, __FILE__ ), array(
 		'wp-blocks',
 		'wp-i18n',
